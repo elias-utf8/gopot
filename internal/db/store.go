efed3a8756ecfd3122e352514e7aefcf3ecc841c
elias <elias.gauthier@etu.u-bordeaux.fr>
2026-04-23 16:00:40 +0200

    Creating store for db query, clean code brr

---
+++ internal/db/store.go
@@ -0,0 +1,50 @@
+package db
+
+import (
+	"database/sql"
+	"net"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/gliderlabs/ssh"
+	gossh "golang.org/x/crypto/ssh"
+)
+
+type Store struct {
+	db *sql.DB
+}
+
+// Store constructor
+func NewStore(db *sql.DB) *Store {
+	return &Store{db: db}
+}
+
+func (s *Store) InsertSession(se ssh.Session) (int64, error) {
+	host, portStr, _ := net.SplitHostPort(se.RemoteAddr().String())
+	port, _ := strconv.Atoi(portStr)
+
+	var pubkey sql.NullString
+	pk := se.PublicKey()
+	if pk != nil {
+		pubkey.String = strings.TrimSpace(string(gossh.MarshalAuthorizedKey(pk)))
+		pubkey.Valid = true
+	}
+
+	res, err := s.db.Exec(
+		`INSERT INTO sessions (user, remote_ip, remote_port, pubkey, started_at) VALUES (?, ?, ?, ?, ?)`,
+		se.User(), host, port, pubkey, time.Now().UTC().Format(time.RFC3339Nano),
+	)
+	if err != nil {
+		return 0, err
+	}
+	return res.LastInsertId()
+}
+
+func (s *Store) EndSession(id int64) error {
+	_, err := s.db.Exec(
+		`UPDATE sessions SET ended_at = ? WHERE id = ?`,
+		time.Now().UTC().Format(time.RFC3339Nano), id,
+	)
+	return err
+}
